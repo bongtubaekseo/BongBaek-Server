5facc367eec176104c5847af8e2caa43cec036f8
kymin <park48664866@gmail.com>
2025-06-24 15:15:16 +0900

    [INIT] 프로젝트 생성

---
+++ src/main/java/org/appjam/bongbeak/BongbeakApplication.java
@@ -0,0 +1,13 @@
+package org.appjam.bongbeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BongbeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BongbeakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/appjam/bongbeak/BongbeakApplicationTests.java
@@ -0,0 +1,13 @@
+package org.appjam.bongbeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BongbeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
