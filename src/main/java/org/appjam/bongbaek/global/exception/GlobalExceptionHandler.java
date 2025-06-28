365a0c28defaad8bf58d63446b2dba97ab0dec2a
chounjae <blueskycuj@naver.com>
2025-06-28 11:52:03 +0900

    [FIX] #2 상태 코드 불일치 해결

--- src/main/java/org/appjam/bongbaek/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/appjam/bongbaek/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package org.appjam.bongbaek.global.exception;
 
+import com.google.protobuf.Api;
 import org.appjam.bongbaek.global.api.ApiResponse;
 import org.appjam.bongbaek.global.common.CommonErrorCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,22 +13,30 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ApiResponse<Void> handleSampleException(CustomException e) {
-        return ApiResponse.failure(e.getErrorCode());
+    public ResponseEntity<ApiResponse<Void>> handleSampleException(CustomException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(ApiResponse.failure(e.getErrorCode()));
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ApiResponse<Void> handleNoHandlerFoundException(NoHandlerFoundException e) {
-        return ApiResponse.failure(CommonErrorCode.INVALID_URL_ERROR);
+    public ResponseEntity<ApiResponse<Void>> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        return ResponseEntity
+                .status(CommonErrorCode.INVALID_URL_ERROR.getStatus())
+                .body(ApiResponse.failure(CommonErrorCode.INVALID_URL_ERROR));
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ApiResponse<Void> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-        return ApiResponse.failure(CommonErrorCode.METHOD_NOT_ALLOWED_ERROR);
+    public ResponseEntity<ApiResponse<Void>> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity
+                .status(CommonErrorCode.METHOD_NOT_ALLOWED_ERROR.getStatus())
+                .body(ApiResponse.failure(CommonErrorCode.METHOD_NOT_ALLOWED_ERROR));
     }
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<Void> handleInternalServerException(Exception e) {
-        return ApiResponse.failure(CommonErrorCode.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ApiResponse<Void>> handleInternalServerException(Exception e) {
+        return ResponseEntity
+                .status(CommonErrorCode.INTERNAL_SERVER_ERROR.getStatus())
+                .body(ApiResponse.failure(CommonErrorCode.INTERNAL_SERVER_ERROR));
     }
 }
