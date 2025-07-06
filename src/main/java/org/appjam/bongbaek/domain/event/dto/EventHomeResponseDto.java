0e9df4559c6565aab5a9cf2d4ab454caf85e6172
chounjae <blueskycuj@naver.com>
2025-07-07 01:20:24 +0900

    [FEAT] 홈 화면 조회를 위한 dto 작성

---
+++ src/main/java/org/appjam/bongbaek/domain/event/dto/EventHomeResponseDto.java
@@ -0,0 +1,48 @@
+package org.appjam.bongbaek.domain.event.dto;
+
+import static org.appjam.bongbaek.domain.event.dto.EventCommonDto.*;
+import org.appjam.bongbaek.domain.event.entity.Event;
+
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class EventHomeResponseDto {
+
+    private HostInfo hostInfo;
+    private EventInfo eventInfo;
+    private LocationInfo locationInfo;
+
+    public static EventHomeResponseDto of(Event event) {
+
+        HostInfo hostInfo = HostInfo.builder()
+                .hostName(event.getHostName())
+                .hostNickname(event.getHostNickname())
+                .build();
+
+        EventInfo eventInfo = EventInfo.builder()
+                .eventId(event.getEventId().toString())
+                .eventCategory(event.getEventCategory())
+                .eventRelationship(event.getRelationship())
+                .cost(event.getCost())
+                .isAttend(null)                  // null 입력 시 JSON에서 제외
+                .eventDate(event.getEventDate())
+                .note(null)
+                .build();
+
+        LocationInfo locationInfo = LocationInfo.builder()
+                .location(event.getLocation())
+                .address(null)
+                .longitude(null)
+                .latitude(null)
+                .build();
+
+        return EventHomeResponseDto.builder()
+                .hostInfo(hostInfo)
+                .eventInfo(eventInfo)
+                .locationInfo(locationInfo)
+                .build();
+    }
+}
