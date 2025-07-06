be520e253fbcce823da066ec811dcd966cd42e2b
chounjae <blueskycuj@naver.com>
2025-07-07 01:20:49 +0900

    [FEAT] 경조사 상세 조회를 위한 dto 작성

---
+++ src/main/java/org/appjam/bongbaek/domain/event/dto/EventDetailResponseDto.java
@@ -0,0 +1,48 @@
+package org.appjam.bongbaek.domain.event.dto;
+
+import static org.appjam.bongbaek.domain.event.dto.EventCommonDto.*;
+import org.appjam.bongbaek.domain.event.entity.Event;
+
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class EventDetailResponseDto {
+
+    private HostInfo hostInfo;
+    private EventInfo eventInfo;
+    private LocationInfo locationInfo;
+
+    public static EventDetailResponseDto of(Event event) {
+
+        HostInfo hostInfo = HostInfo.builder()
+                .hostName(event.getHostName())
+                .hostNickname(event.getHostNickname())
+                .build();
+
+        EventInfo eventInfo = EventInfo.builder()
+                .eventId(event.getEventId().toString())
+                .eventCategory(event.getEventCategory())
+                .eventRelationship(event.getRelationship())
+                .cost(event.getCost())
+                .isAttend(event.isAttended())
+                .eventDate(event.getEventDate())
+                .note(event.getNote())
+                .build();
+
+        LocationInfo locationInfo = LocationInfo.builder()
+                .location(event.getLocation())
+                .address(event.getAddress())
+                .longitude(event.getLongitude())
+                .latitude(event.getLatitude())
+                .build();
+
+        return EventDetailResponseDto.builder()
+                .hostInfo(hostInfo)
+                .eventInfo(eventInfo)
+                .locationInfo(locationInfo)
+                .build();
+    }
+}
