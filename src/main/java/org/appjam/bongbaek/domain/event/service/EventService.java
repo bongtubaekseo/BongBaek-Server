7b15d1a0e11605c57dba7501d2bb0590dd1b5c57
chounjae <blueskycuj@naver.com>
2025-07-07 20:56:37 +0900

    [FEAT] 경조사 수정 메서드 작성

--- src/main/java/org/appjam/bongbaek/domain/event/service/EventService.java
+++ src/main/java/org/appjam/bongbaek/domain/event/service/EventService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.appjam.bongbaek.domain.event.util.EventValidator.EventAuthorization;
-import org.appjam.bongbaek.domain.event.dto.EventDetailResponseDto;
+
+import org.appjam.bongbaek.domain.event.dto.request.EventUpdateRequestDto;
+import org.appjam.bongbaek.domain.event.dto.response.EventDetailResponseDto;
 import org.springframework.transaction.annotation.Transactional;
-import org.appjam.bongbaek.domain.event.dto.EventHomeResponseDto;
+import org.appjam.bongbaek.domain.event.dto.response.EventHomeResponseDto;
 import org.appjam.bongbaek.domain.event.entity.Event;
 import org.appjam.bongbaek.domain.event.exception.NotFoundEventException;
 import org.appjam.bongbaek.domain.event.repository.EventRepository;
@@ -47,6 +49,17 @@ public class EventService {
         return events.stream()
                 .map(EventHomeResponseDto::of)
                 .toList();
+    }
+
+    @Transactional
+    public void updateEventByEventId(UUID eventId, UUID memberUUID, EventUpdateRequestDto request) {
+
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(NotFoundEventException::new);
+
+        EventAuthorization(memberUUID, event.getMember().getMemberId());
+
+        event.updateFromDto(request);
     }
 
     @Transactional
