812fd72568c2ce0cab9f2f3e255e746dbbb60e7b
chounjae <blueskycuj@naver.com>
2025-07-08 00:40:03 +0900

    [FEAT] request 수정, 삭제 dto 작성

--- src/main/java/org/appjam/bongbaek/domain/event/dto/request/EventUpdateRequestDto.java
+++ src/main/java/org/appjam/bongbaek/domain/event/dto/request/EventUpdateRequestDto.java
@@ -1,10 +1,19 @@
 package org.appjam.bongbaek.domain.event.dto.request;
 
 import org.appjam.bongbaek.domain.event.dto.common.*;
+import org.appjam.bongbaek.domain.event.entity.Category;
+import org.appjam.bongbaek.domain.event.entity.Relationship;
 
 public record EventUpdateRequestDto (
         HostInfo hostInfo,
         EventInfo eventInfo,
         LocationInfo locationInfo
 ){
+    public Category getCategoryEnum() {
+        return Category.of(eventInfo.eventCategory());
+    }
+
+    public Relationship getRelationshipEnum() {
+        return Relationship.of(eventInfo.relationship());
+    }
 }
