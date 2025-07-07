ef5b719bc4dd593c6dc605082d289bd415bfaf9f
minkyeong <haileyre000b@naver.com>
2025-07-07 14:10:05 +0900

    [FEAT] MemberAuthentication 작성

---
+++ src/main/java/org/appjam/bongbaek/global/jwt/MemberAuthentication.java
@@ -0,0 +1,17 @@
+package org.appjam.bongbaek.global.jwt;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class MemberAuthentication extends UsernamePasswordAuthenticationToken {
+    public MemberAuthentication(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(principal, credentials, authorities);
+    }
+
+    public static MemberAuthentication createMemberAuthentication(UUID memberId) {
+        return new MemberAuthentication(memberId, null, null); // TODO: 권한 레벨 현재 X. 필요시 추가 가능
+    }
+}
