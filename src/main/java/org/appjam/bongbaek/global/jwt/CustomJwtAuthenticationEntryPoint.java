37d773e99874306ed95921adf1377841b226e932
minkyeong <haileyre000b@naver.com>
2025-07-07 14:12:17 +0900

    [FEAT] jwt 관련 예외 처리 추가

---
+++ src/main/java/org/appjam/bongbaek/global/jwt/CustomJwtAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package org.appjam.bongbaek.global.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.appjam.bongbaek.global.api.ApiResponse;
+import org.appjam.bongbaek.global.common.CommonErrorCode;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomJwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        setResponse(response);
+    }
+
+    private void setResponse(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter()
+                .write(objectMapper.writeValueAsString(ApiResponse.failure(CommonErrorCode.UNAUTHORIZED)));
+    }
+}
