f5f0a039e7a4bf485eca7a0d6c9fdb02d1eb5261
chounjae <blueskycuj@naver.com>
2025-07-07 20:26:29 +0900

    [CHORE] 경조사 응답 코드 추가

--- src/main/java/org/appjam/bongbaek/global/common/CommonSuccessCode.java
+++ src/main/java/org/appjam/bongbaek/global/common/CommonSuccessCode.java
@@ -7,9 +7,16 @@ import org.springframework.http.HttpStatus;
 public enum CommonSuccessCode implements SuccessCode {
     // 200 Ok
     OK(true, HttpStatus.OK, "요청이 성공했습니다."),
+    GET_EVENT(true, HttpStatus.OK, "경조사가 성공적으로 조회됐습니다."),
+    UPDATED_EVENT(true, HttpStatus.OK, "경조사가 성공적으로 수정됐습니다."),
+    DELETED_EVENT(true, HttpStatus.OK, "경조사가 성공적으로 삭제됐습니다."),
 
     // 201 Created
-    CREATED(true, HttpStatus.CREATED, "생성되었습니다.");
+    CREATED(true, HttpStatus.CREATED, "생성되었습니다."),
+    CREATED_EVENT(true, HttpStatus.CREATED, "경조사가 성공적으로 생성됐습니다."),
+
+    // 204 No Content
+    NO_EVENT(true, HttpStatus.NO_CONTENT,"등록된 경조사가 없습니다.");
 
     private final boolean success;
     private final HttpStatus status;
