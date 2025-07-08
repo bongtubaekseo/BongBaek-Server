56ec5668efc4a9ae846d01a0350faf7304ac90b6
chounjae <blueskycuj@naver.com>
2025-07-08 21:22:51 +0900

    [REFACTOR] 데이터 무결성을 위한 JPA에서 유효성 검증

--- src/main/java/org/appjam/bongbaek/domain/event/repository/EventRepository.java
+++ src/main/java/org/appjam/bongbaek/domain/event/repository/EventRepository.java
@@ -2,6 +2,7 @@ package org.appjam.bongbaek.domain.event.repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.appjam.bongbaek.domain.event.entity.Event;
@@ -10,6 +11,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
+
     List<Event> findTop3ByEventDateGreaterThanEqualAndMemberMemberIdOrderByEventDateAsc(LocalDate now, UUID member_memberId); // TO DO: jwt 발급 시 사용
-    List<Event> findTop3ByEventDateAfterOrderByEventDateDesc(LocalDate date);   // TEST용. 멤버 JWT 구현 시 삭제
+
+    Optional<Event> findEventByEventIdAndMemberMemberId(UUID eventId, UUID memberMemberId);
+
+    List<Event> findAllByEventIdInAndMemberMemberId(List<UUID> eventIds, UUID memberId);
 }
