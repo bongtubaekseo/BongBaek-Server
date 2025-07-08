db610b89a398825dd2cd47c91b58d0ca980b5b2b
chounjae <blueskycuj@naver.com>
2025-07-08 13:31:38 +0900

    [FILE] 코드 순서 리팩터링

--- src/main/java/org/appjam/bongbaek/domain/event/code/EventErrorCode.java
+++ src/main/java/org/appjam/bongbaek/domain/event/code/EventErrorCode.java
@@ -8,8 +8,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum EventErrorCode implements ErrorCode {
-    EVENT_NOT_FOUND(true, HttpStatus.NOT_FOUND,  "경조사를 찾을 수 없습니다."),
-    INVALID_EVENT_DATE(false, HttpStatus.BAD_REQUEST, "유효하지 않은 경조사 날짜입니다.");
+
+    // 400
+    INVALID_EVENT_DATE(false, HttpStatus.BAD_REQUEST, "유효하지 않은 경조사 날짜입니다."),
+    INVALID_UUID_FORMAT(false, HttpStatus.BAD_REQUEST, "유효하지 않은 UUID 포멧입니다."),
+
+    // 404
+    EVENT_NOT_FOUND(true, HttpStatus.NOT_FOUND,  "경조사를 찾을 수 없습니다.");
 
     private final boolean success;
     private final HttpStatus status;
--- src/main/java/org/appjam/bongbaek/domain/event/code/EventSuccessCode.java
+++ src/main/java/org/appjam/bongbaek/domain/event/code/EventSuccessCode.java
@@ -8,9 +8,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum EventSuccessCode implements SuccessCode {
+
+    // 200
     GET_EVENT(true,HttpStatus.OK, "경조사가 성공적으로 조회됐습니다."),
     UPDATED_EVENT(true, HttpStatus.OK, "경조사가 성공적으로 수정됐습니다."),
     DELETED_EVENT(true, HttpStatus.OK, "경조사가 성공적으로 삭제됐습니다."),
+
+    //204
     NO_EVENT(true, HttpStatus.NO_CONTENT, "등록된 경조사가 없습니다.");
 
     private final boolean success;
