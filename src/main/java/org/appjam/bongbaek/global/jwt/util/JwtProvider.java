19f7c78178c36612596a2a3b7c69addf8b6ad4bc
minkyeong <haileyre000b@naver.com>
2025-07-09 15:27:13 +0900

    [FEAT] JwtProvider 수정

--- src/main/java/org/appjam/bongbaek/global/jwt/util/JwtProvider.java
+++ src/main/java/org/appjam/bongbaek/global/jwt/util/JwtProvider.java
@@ -1,10 +1,8 @@
 package org.appjam.bongbaek.global.jwt.util;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
-import org.appjam.bongbaek.global.jwt.dto.TokenResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -22,25 +20,13 @@ public class JwtProvider {
 
     private final JwtParser jwtParser;
 
-    public TokenResponse reissueToken(UUID memberId) {
-        return TokenResponse.of(
-                generateAccessToken(memberId),
-                generateRefreshToken(memberId)
-        );
-    } // NOTE: TokenResponse로 반환
-
     private String generateToken(UUID memberId, Long expirationTime) {
         final Date now = new Date();
-        final Claims claims = Jwts.claims()
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + expirationTime))
-                .build();
-
-        claims.put(MEMBER_ID, memberId.toString());
 
         return Jwts.builder()
-                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                .setClaims(claims)
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + expirationTime))
+                .claim(MEMBER_ID, memberId.toString())
                 .signWith(jwtParser.getSigningKey())
                 .compact();
     }
