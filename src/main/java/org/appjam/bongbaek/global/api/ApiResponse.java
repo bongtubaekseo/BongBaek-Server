776686db27436d2645be90582936f1151652d9a6
chounjae <blueskycuj@naver.com>
2025-07-11 16:54:09 +0900

    [FEAT] 자바 예외 메시지용 응답 형식 추가

--- src/main/java/org/appjam/bongbaek/global/api/ApiResponse.java
+++ src/main/java/org/appjam/bongbaek/global/api/ApiResponse.java
@@ -11,17 +11,23 @@ public record ApiResponse<T>(
         @JsonInclude(value = JsonInclude.Include.NON_NULL)
         T data
 ) {
-    // response body 없는 버전
+    // NOTE: response body 없는 버전
     public static <T> ApiResponse<T> success(SuccessCode successCode) {
         return new ApiResponse<>(successCode.getSuccess(), successCode.getStatus().value(), successCode.getMessage(), null);
     }
 
-    // response body 있는 버전
+    // NOTE: response body 있는 버전
     public static <T> ApiResponse<T> success(SuccessCode successCode, T data) {
         return new ApiResponse<>(successCode.getSuccess(), successCode.getStatus().value(), successCode.getMessage(), data);
     }
 
+    // NOTE: Custom 에러 메시지 버전
     public static <T> ApiResponse<T> failure(ErrorCode failureCode) {
         return new ApiResponse<>(failureCode.getSuccess(), failureCode.getStatus().value(), failureCode.getMessage(), null);
+    }
+
+    // NOTE: Exception Handler 버전
+    public static <T> ApiResponse<T> failure(String message) {
+        return new ApiResponse<>(false, 400, message, null);
     }
 }
