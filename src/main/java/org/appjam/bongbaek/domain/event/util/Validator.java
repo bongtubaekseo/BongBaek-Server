1cf39c7347cf4c1a85460392d436c45a5a71436e
chounjae <blueskycuj@naver.com>
2025-07-11 16:36:51 +0900

    [FEAT] 노트 예외 메시지 추가

---
+++ src/main/java/org/appjam/bongbaek/domain/event/util/Validator.java
@@ -0,0 +1,28 @@
+package org.appjam.bongbaek.global.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+    // 하나의 시각적 유니코드 grapheme의 정규식인 "\X"를 이용하여 문자열의 길이를 카운트
+    private static final Pattern GRAPHEME_PATTERN = Pattern.compile("\\X");
+
+    public static boolean isEmpty(String string) {
+        return string.isBlank();
+    }
+
+    public static boolean isOverLength(String string, int maxLength) {
+        return lengthWithEmoji(string) > maxLength;
+    }
+
+    private static int lengthWithEmoji(String string) {
+        Matcher matcher = GRAPHEME_PATTERN.matcher(string);
+        int count = 0;
+
+        while (matcher.find()) {
+            count++;
+        }
+
+        return count;
+    }
+}
