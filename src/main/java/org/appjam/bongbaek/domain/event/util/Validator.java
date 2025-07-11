28307812c8610db15614473c4718f2cb30537611
chounjae <blueskycuj@naver.com>
2025-07-11 16:39:22 +0900

    [FEAT] 노트 이모지 포함 50자 검증

--- src/main/java/org/appjam/bongbaek/domain/event/util/Validator.java
+++ src/main/java/org/appjam/bongbaek/domain/event/util/Validator.java
@@ -1,4 +1,6 @@
-package org.appjam.bongbaek.global.util;
+package org.appjam.bongbaek.domain.event.util;
+
+import org.appjam.bongbaek.domain.event.exception.InvalidNoteException;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,12 +9,14 @@ public class Validator {
     // 하나의 시각적 유니코드 grapheme의 정규식인 "\X"를 이용하여 문자열의 길이를 카운트
     private static final Pattern GRAPHEME_PATTERN = Pattern.compile("\\X");
 
-    public static boolean isEmpty(String string) {
-        return string.isBlank();
+    public static Boolean isEmpty(String note) {
+        return note.isBlank();
     }
 
-    public static boolean isOverLength(String string, int maxLength) {
-        return lengthWithEmoji(string) > maxLength;
+    public static void validateLength(String note) {
+        if (lengthWithEmoji(note) > 50) {
+            throw new InvalidNoteException();
+        }
     }
 
     private static int lengthWithEmoji(String string) {
