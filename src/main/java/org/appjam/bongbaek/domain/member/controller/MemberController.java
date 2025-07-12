023875078fbbe8c10e3ccbd465de3571da512b2c
minkyeong <haileyre000b@naver.com>
2025-07-12 22:07:08 +0900

    feat: reissue 관련 controller 추가

--- src/main/java/org/appjam/bongbaek/domain/member/controller/MemberController.java
+++ src/main/java/org/appjam/bongbaek/domain/member/controller/MemberController.java
@@ -3,13 +3,13 @@ package org.appjam.bongbaek.domain.member.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.appjam.bongbaek.global.oauth.PrincipalHandler;
 import org.appjam.bongbaek.domain.member.dto.LoginRequest;
 import org.appjam.bongbaek.domain.member.dto.LoginResponse;
 import org.appjam.bongbaek.domain.member.dto.SignUpRequest;
 import org.appjam.bongbaek.domain.member.service.MemberService;
 import org.appjam.bongbaek.global.api.ApiResponse;
 import org.appjam.bongbaek.global.common.CommonSuccessCode;
+import org.appjam.bongbaek.global.jwt.dto.TokenResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -47,5 +47,16 @@ public class MemberController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success(CommonSuccessCode.SIGNUP_COMPLETED, loginResponse));
+    }
+
+    @Operation(summary = "토큰 재발급", description = "리프레시 토큰을 사용하여 새로운 액세스 토큰과 리프레시 토큰을 발급받습니다.")
+    @PostMapping("/member/reissue")
+    public ResponseEntity<ApiResponse<TokenResponse>> reissueTokens(
+            @RequestHeader("refreshToken") final String refreshToken
+    ) {
+        TokenResponse tokenResponse = memberService.reissueTokens(refreshToken);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(CommonSuccessCode.TOKEN_REISSUED, tokenResponse));
     }
 }
