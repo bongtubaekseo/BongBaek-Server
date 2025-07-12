1e0e09f46876ce0dddef5b2c60b0250cf3576f00
minkyeong <haileyre000b@naver.com>
2025-07-12 17:03:45 +0900

    feat: 사용하지 않는 로직 제거, npe 개선

--- src/main/java/org/appjam/bongbaek/global/oauth/kakao/KakaoLoginClient.java
+++ src/main/java/org/appjam/bongbaek/global/oauth/kakao/KakaoLoginClient.java
@@ -1,63 +1,13 @@
 package org.appjam.bongbaek.global.oauth.kakao;
 
 import lombok.extern.slf4j.Slf4j;
-import org.appjam.bongbaek.global.oauth.kakao.dto.KakaoAccessTokenResponse;
 import org.appjam.bongbaek.global.oauth.kakao.dto.KakaoInfoResponse;
-import org.appjam.bongbaek.global.oauth.kakao.dto.KakaoLoginResult;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 @Slf4j
 @Component
 public class KakaoLoginClient {
-
-    @Value("${kakao.client-id}")
-    private String kakaoClientId;
-    @Value("${kakao.redirect-url}")
-    private String kakaoRedirectUrl;
-
-    /**
-     * 테스트용 메서드 - 인가코드로 토큰 발급
-     * 현재는 클라이언트에서 직접 토큰을 발급받아 전달하므로 사용되지 않음
-     */
-    @Deprecated
-    public KakaoLoginResult login(final String code) {
-
-        RestClient restClient = RestClient.create();
-        URI uri = UriComponentsBuilder
-                .fromUriString("https://kauth.kakao.com/oauth/token")
-                .queryParam("code", "{code}")
-                .queryParam("client_id", "{client_id}")
-                .queryParam("redirect_uri", "{redirect_uri}")
-                .queryParam("grant_type", "{grant_type}")
-                .encode()
-                .buildAndExpand(code, kakaoClientId, kakaoRedirectUrl, "authorization_code")
-                .toUri();
-
-        KakaoAccessTokenResponse kakaoAccessTokenResponse = restClient.post()
-                .uri(uri)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .retrieve()
-                .body(KakaoAccessTokenResponse.class);
-
-
-        log.info(kakaoAccessTokenResponse.accessToken());
-
-        KakaoInfoResponse kakaoInfoResponse = restClient.get()
-                .uri("https://kapi.kakao.com/v2/user/me")
-                .header("Authorization", "Bearer " + kakaoAccessTokenResponse.accessToken())
-                .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
-                .retrieve()
-                .body(KakaoInfoResponse.class);
-
-        return KakaoLoginResult.of(Long.parseLong(kakaoInfoResponse.id()), kakaoAccessTokenResponse.accessToken());
-    } // NOTE: kakaoId 와 kakaoAccessToken을 반환
-
 
     public Long validateKakaoAccessToken(final String accessToken) {
         try {
@@ -69,6 +19,10 @@ public class KakaoLoginClient {
                     .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
                     .retrieve()
                     .body(KakaoInfoResponse.class);
+
+            if (kakaoInfoResponse == null || kakaoInfoResponse.id() == null) {
+                throw new IllegalArgumentException("카카오 사용자 정보를 가져올 수 없습니다.");
+            }
 
             return Long.parseLong(kakaoInfoResponse.id());
         } catch (Exception e) {
