e5e1e778915843c2240767732c25d9fda3862fdf
chounjae <blueskycuj@naver.com>
2025-07-13 15:43:41 +0900

    [FIX] JWT 토큰 받도록 변경

--- src/main/java/org/appjam/bongbaek/domain/event/controller/EventController.java
+++ src/main/java/org/appjam/bongbaek/domain/event/controller/EventController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +40,7 @@ public class EventController {
     private final EventService eventService;
 
     @PostMapping
+
     public ResponseEntity<ApiResponse<ApiResponse.EmptyBody>> createEvent(
             @AuthenticationPrincipal final UUID memberId,
             @RequestBody @Valid final EventWriteDto eventWriteDto
