968e6d9f996316bef091146ec269a582564ee443
chounjae <blueskycuj@naver.com>
2025-07-15 20:35:30 +0900

    [FIX] hostName, hostNickname 숫자 허용

--- src/main/java/org/appjam/bongbaek/domain/event/dto/common/HostInfo.java
+++ src/main/java/org/appjam/bongbaek/domain/event/dto/common/HostInfo.java
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record HostInfo(
-		@Pattern(regexp = "^[가-힣a-zA-Z\\s]+$", message = "이름은 한글, 영어 대소문자, 공백으로만 입력 가능합니다")
-		@Size(min = 2, max = 10, message = "2자 이상 10자 이내만 기입할 수 있습니다")
+		@Pattern(regexp = "^[가-힣a-zA-Z0-9\\s]+$", message = "특수문자는 기입할 수 없어요")
+		@Size(min = 2, max = 10, message = "2자 이상 10자 이내만 기입할 수 있어요")
 		String hostName,
-		@Pattern(regexp = "^[가-힣a-zA-Z\\s]+$", message = "별명은 한글, 영어 대소문자, 공백으로만 입력 가능합니다")
-		@Size(min = 2, max = 10, message = "2자 이상 10자 이내만 기입할 수 있습니다")
+		@Pattern(regexp = "^[가-힣a-zA-Z0-9\\s]+$", message = "특수문자는 기입할 수 없어요")
+		@Size(min = 2, max = 10, message = "2자 이상 10자 이내만 기입할 수 있어요")
 		String hostNickname) {
 
 	public static HostInfo from(Event event) {
