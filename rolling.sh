c63dd69750cef165dfea72275686d2a84af6422b
chounjae <blueskycuj@naver.com>
2025-08-28 00:28:54 +0900

    [FIX] 롤백 명령 수정

--- rolling.sh
+++ rolling.sh
@@ -4,55 +4,46 @@ APP_NAME="bongbaek-server"
 BLUE="bongbaek1-container"
 GREEN="bongbaek2-container"
 
+# rolling 사용시 bongbaek-server 이미지 태그를 버전별로 관리해야함.
+PREV_VERSION="1.0.0"
+DEPLOY_VERSION="1.0.1"
+
 # DOCKER 로그인
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # 이미지 가져오기
-docker pull $DOCKER_USERNAME/$APP_NAME:latest
-
-# 현재 실행 중인 이미지 저장
-CURRENT_CONTAINER=$(docker inspect -f '{{.Image}}' $GREEN 2>/dev/null)
+docker pull $DOCKER_USERNAME/$APP_NAME:"$DEPLOY_VERSION"
 
 # Redis & Dozzle 실행
 docker compose up -d redis dozzle
 
-# 새 버전 컨테이너 GREEN에서 실행
-docker compose up -d $GREEN
+# 새 버전 컨테이너 실행
+for CONTAINER in "$GREEN" "$BLUE"; do
 
-timeout=120
-count=0
-until [ "$(docker inspect -f '{{.State.Health.Status}}' $GREEN)" = "healthy" ] || [ $count -ge $timeout ]; do
-  sleep 5
-  count=$((count+5))
-done
+  docker compose stop "$CONTAINER" || true
+  docker compose rm -f "$CONTAINER" || true
 
-if [ "$(docker inspect -f '{{.State.Health.Status}}' $GREEN)" != "healthy" ]; then
-  echo "헬스 체크 실패 → 롤백"
-  if [ -n "$CURRENT_CONTAINER" ]; then
-    docker run -d --name $GREEN $CURRENT_CONTAINER || true
+  APP_VERSION="$DEPLOY_VERSION" docker compose up -d "$CONTAINER"
+
+  timeout=120
+  count=0
+  until [ "$(docker inspect -f '{{.State.Health.Status}}' "$CONTAINER")" = "healthy" ] || [ $count -ge $timeout ]; do
+    sleep 5
+    count=$((count+5))
+  done
+
+  if [ "$(docker inspect -f '{{.State.Health.Status}}' "$CONTAINER")" != "healthy" ]; then
+    echo "헬스 체크 실패 → 롤백"
+
+    docker stop "$CONTAINER" || true
+    docker rm "$CONTAINER" || true
+
+    APP_VERSION="$PREV_VERSION" docker compose up -d "$CONTAINER"
+    exit 1
   fi
-  exit 1
-fi
 
-echo "헬스 체크 성공"
-
-# 새 버전 컨테이너 BLUE에서 실행
-docker compose up -d $BLUE
-
-timeout=120
-count=0
-until [ "$(docker inspect -f '{{.State.Health.Status}}' $BLUE)" = "healthy" ] || [ $count -ge $timeout ]; do
-  sleep 5
-  count=$((count+5))
+  echo "헬스 체크 성공"
 done
-
-if [ "$(docker inspect -f '{{.State.Health.Status}}' $BLUE)" != "healthy" ]; then
-  echo "헬스 체크 실패 → 롤백"
-  if [ -n "$CURRENT_CONTAINER" ]; then
-    docker run -d --name $BLUE $CURRENT_CONTAINER || true
-  fi
-  exit 1
-fi
 
 # Nginx 트래픽 전환
 docker compose up -d nginx
