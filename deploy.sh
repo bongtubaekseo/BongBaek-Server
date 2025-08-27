17d4d4167db613957c41bce46517c91f963e2e16
chounjae <blueskycuj@naver.com>
2025-08-28 00:29:16 +0900

    [FIX] 이미지 하드코딩 수정

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@ APP_NAME="bongbaek-server"
 OLD_CONTAINER="bongbaek1-container"
 
 # DOCKER 로그인
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # 실행중인 컨테이너 확인
 if docker ps --format '{{.Names}}' | grep -q "$OLD_CONTAINER"; then
-  CURRENT="bongbaek1"
-  IDLE="bongbaek2"
+  CURRENT="bongbaek1-container"
+  IDLE="bongbaek2-container"
 else
-  CURRENT="bongbaek2"
-  IDLE="bongbaek1"
+  CURRENT="bongbaek2-container"
+  IDLE="bongbaek1-container"
 fi
 
 # 이미지 가져오기
@@ -22,20 +22,20 @@ docker pull $DOCKER_USERNAME/$APP_NAME:latest
 docker compose up -d redis dozzle
 
 # 새 버전 컨테이너 실행
-docker compose up -d $IDLE
+docker compose up -d "$IDLE"
 
 # 새 컨테이너 헬스체크
 timeout=120
 count=0
-until [ "$(docker inspect -f '{{.State.Health.Status}}' ${IDLE}-container)" = "healthy" ] || [ $count -ge $timeout ]; do
+until [ "$(docker inspect -f '{{.State.Health.Status}}' "$IDLE")" = "healthy" ] || [ $count -ge $timeout ]; do
   sleep 5
   count=$((count+5))
 done
 
-if [ "$(docker inspect -f '{{.State.Health.Status}}' ${IDLE}-container)" != "healthy" ]; then
+if [ "$(docker inspect -f '{{.State.Health.Status}}' "$IDLE")" != "healthy" ]; then
   echo "헬스 체크 실패"
-  docker stop ${IDLE}-container
-  docker rm ${IDLE}-container
+  docker stop "$IDLE"
+  docker rm "$IDLE"
   exit 1
 fi
 
@@ -45,8 +45,8 @@ echo "헬스 체크 성공"
 docker compose up -d nginx
 
 # 기존 컨테이너 종료
-docker stop ${CURRENT}-container
-docker rm ${CURRENT}-container
+docker stop "$CURRENT" || true
+docker rm "$CURRENT" || true
 
 docker image prune -f
 
