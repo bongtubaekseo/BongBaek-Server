59c5b37e8532e5b2dfc6da5a10234e748007171a
chounjae <blueskycuj@naver.com>
2025-08-29 19:55:41 +0900

    [FEAT] 롤링 배포 구현

--- deploy.sh
+++ deploy.sh
@@ -1,52 +1,51 @@
 set -e
 
 APP_NAME="bongbaek-server"
-OLD_CONTAINER="blue"
+FIRST="bongbaek1"
+SECOND="bongbaek2"
+CONTAINER="-container"
+
+# 버전 정보 가져오기
+DEPLOY_VERSION=$(tr -d '\r' < version)
+
+PREV_VERSION=$(docker inspect --format='{{index .Config.Image}}' "$FIRST$CONTAINER" 2>/dev/null | awk -F: '{print $2}')
+
+export APP_VERSION="$DEPLOY_VERSION"
 
 # DOCKER 로그인
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# 실행중인 컨테이너 확인
-if docker ps --format '{{.Names}}' | grep -q "$OLD_CONTAINER"; then
-  CURRENT="blue"
-  IDLE="green"
-else
-  CURRENT="green"
-  IDLE="blue"
-fi
-
 # 이미지 가져오기
-docker pull $DOCKER_USERNAME/$APP_NAME:latest
-
-# Redis & Dozzle 실행
-docker compose up -d redis dozzle
+docker pull "$DOCKER_USERNAME/$APP_NAME:$DEPLOY_VERSION"
 
 # 새 버전 컨테이너 실행
-docker compose up -d "$IDLE"
+for SERVICE in "$FIRST" "$SECOND"; do
 
-# 새 컨테이너 헬스체크
-timeout=120
-count=0
-until [ "$(docker inspect -f '{{.State.Health.Status}}' "$IDLE")" = "healthy" ] || [ $count -ge $timeout ]; do
-  sleep 5
-  count=$((count+5))
+  docker stop "$SERVICE$CONTAINER" || true
+  docker rm -f "$SERVICE$CONTAINER" || true
+  docker compose up -d "$SERVICE"
+
+  timeout=120
+  count=0
+  until [ "$(docker inspect -f '{{.State.Health.Status}}' "$SERVICE$CONTAINER")" = "healthy" ] || [ $count -ge $timeout ]; do
+    sleep 5
+    count=$((count+5))
+  done
+
+  if [ "$(docker inspect -f '{{.State.Health.Status}}' "$SERVICE$CONTAINER")" != "healthy" ]; then
+    echo "헬스 체크 실패 → 롤백"
+
+    docker stop "$SERVICE$CONTAINER" || true
+    docker rm "$SERVICE$CONTAINER" || true
+
+    APP_VERSION="$PREV_VERSION" docker compose up -d "$SERVICE"
+    exit 1
+  fi
+
+  echo "헬스 체크 성공"
 done
-
-if [ "$(docker inspect -f '{{.State.Health.Status}}' "$IDLE")" != "healthy" ]; then
-  echo "헬스 체크 실패"
-  docker stop ${IDLE}
-  docker rm ${IDLE}
-  exit 1
-fi
-
-echo "헬스 체크 성공"
 
 # Nginx 트래픽 전환
 docker compose up -d nginx
 
-# 기존 컨테이너 종료
-docker stop "${CURRENT}"
-docker rm "${CURRENT}"
-
 docker image prune -f
-
