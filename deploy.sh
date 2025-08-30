2f1fb43aecfea9c3905fb3c624b7c9ea4419ea49
kymin <park48664866@gmail.com>
2025-08-30 18:15:51 +0900

    [HOTFIX] 엔진엑스 컨테이너 의존성 제거

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # 서비스가 실행 중이면 그대로 두기, 아니면 재시작
-docker compose up -d --no-recreate redis prometheus grafana mysqld_exporter redis_exporter dozzle
+docker compose up -d --no-recreate nginx nginx-exporter redis prometheus grafana mysqld_exporter redis_exporter dozzle
 
 # bongbaek 서비스 롤링 배포
 for service in bongbaek1 bongbaek2
@@ -24,8 +24,6 @@ do
     done
     echo "  $service is healthy!"
 done
-
-docker compose up -d --no-recreate nginx nginx-exporter
 
 # 4. 불필요한 이미지 정리
 docker image prune -f
