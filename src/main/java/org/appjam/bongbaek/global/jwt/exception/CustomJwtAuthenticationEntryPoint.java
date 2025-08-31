a4d5828d543202c5c5ddbfa4e4d9df67aff95626
kyoooooong <haileyre000b@naver.com>
2025-08-31 21:54:26 +0900

    chore: 필요없는 import 제거

--- src/main/java/org/appjam/bongbaek/global/jwt/exception/CustomJwtAuthenticationEntryPoint.java
+++ src/main/java/org/appjam/bongbaek/global/jwt/exception/CustomJwtAuthenticationEntryPoint.java
@@ -1,19 +1,13 @@
 package org.appjam.bongbaek.global.jwt.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.appjam.bongbaek.global.api.ApiResponse;
 import org.appjam.bongbaek.global.common.CommonErrorCode;
 import org.appjam.bongbaek.global.exception.CustomException;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Component
