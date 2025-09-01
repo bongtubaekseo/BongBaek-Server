7b93188d3a5eaf2b9fa95598ddb366abd965cc0f
kyoooooong <haileyre000b@naver.com>
2025-09-02 08:41:21 +0900

    [FEAT] 탈퇴 사유 enum에 description 추가

--- src/main/java/org/appjam/bongbaek/domain/member/enums/WithdrawalReason.java
+++ src/main/java/org/appjam/bongbaek/domain/member/enums/WithdrawalReason.java
@@ -1,9 +1,20 @@
 package org.appjam.bongbaek.domain.member.enums;
 
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@Schema(description = "회원 탈퇴 사유")
 public enum WithdrawalReason {
-    INCONVENIENT,
-    PRIVACY_CONCERN,
-    RARELY_USED,
-    BUG_OR_ERROR,
-    NEW_ACCOUNT
+    INCONVENIENT("이용이 불편함"),
+    PRIVACY_CONCERN("개인정보/보안 우려"),
+    RARELY_USED("이용 빈도가 낮음"),
+    BUG_OR_ERROR("버그/오류 발생"),
+    NEW_ACCOUNT("새 계정 생성"),
+    OTHER("기타(상세 사유 입력 필수)");
+
+    private final String description;
 }
