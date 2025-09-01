661ce6f65e7cdaecd0593cafb697adf6bb12c13c
chounjae <blueskycuj@naver.com>
2025-09-01 22:22:13 +0900

    [FIX] 카카오와 통일성을 위해 이름 수정

--- src/main/java/org/appjam/bongbaek/global/oauth/apple/dto/AppleInfoResponse.java
+++ src/main/java/org/appjam/bongbaek/global/oauth/apple/dto/AppleInfoResponse.java
@@ -3,7 +3,7 @@ package org.appjam.bongbaek.global.oauth.apple.dto;
 import io.jsonwebtoken.Claims;
 
 public record AppleInfoResponse(
-        String sub
+        String id
 ) {
 
     public static AppleInfoResponse of(Claims claims) {
