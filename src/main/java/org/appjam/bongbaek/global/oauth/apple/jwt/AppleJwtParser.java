b053e2bed7a43bf4eba579f0b53c4b2d3b487600
chounjae <blueskycuj@naver.com>
2025-09-02 15:18:07 +0900

    [FIX] 예외처리 세분화

--- src/main/java/org/appjam/bongbaek/global/oauth/apple/jwt/AppleJwtParser.java
+++ src/main/java/org/appjam/bongbaek/global/oauth/apple/jwt/AppleJwtParser.java
@@ -15,6 +15,7 @@ public class AppleJwtParser {
     public Map<String, String> parseHeaders(
             String identityToken
     ){
+        try {
         Header header = Jwts.parser()
                 .build()
                 .parseSignedClaims(identityToken)
@@ -25,6 +26,11 @@ public class AppleJwtParser {
                         Map.Entry::getKey,
                         e -> String.valueOf(e.getValue())
                 ));
+        } catch (MalformedJwtException e) {
+            throw new IllegalArgumentException("잘못된 형식의 JWT 토큰입니다", e);
+        } catch (JwtException e) {
+            throw new IllegalArgumentException("JWT 토큰 파싱 중 오류가 발생했습니다", e);
+        }
     }
 
     public Claims getTokenClaimsByPublicKey(
