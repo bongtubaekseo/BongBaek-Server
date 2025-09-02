6b500612f11689fc2c36e6724dad6b51493e6512
chounjae <blueskycuj@naver.com>
2025-09-02 18:57:51 +0900

    [CHORE] oauth/apple 인증 제외 추가

--- src/main/java/org/appjam/bongbaek/global/config/AuthWhiteList.java
+++ src/main/java/org/appjam/bongbaek/global/config/AuthWhiteList.java
@@ -25,7 +25,7 @@ public class AuthWhiteList {
             "/callback",
 
             // 인증 관련 API
-            "/api/v1/oauth/kakao",
+            "/api/v1/oauth/**",
             "/api/v1/member/profile",
             "/api/v1/member/reissue",
 
