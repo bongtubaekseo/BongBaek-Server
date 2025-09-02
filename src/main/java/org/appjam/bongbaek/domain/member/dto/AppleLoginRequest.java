d1c25b96cccb11bd71f3501ab0d92c510d2fac3f
chounjae <blueskycuj@naver.com>
2025-09-02 19:19:44 +0900

    [FIX] oauth 로그인 dto 통일

--- src/main/java/org/appjam/bongbaek/domain/member/dto/AppleLoginRequest.java
+++
@@ -1,9 +0,0 @@
-package org.appjam.bongbaek.domain.member.dto;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-public record AppleLoginRequest(
-        @Schema(description = "애플에서 발급받은 identity token")
-        String identityToken
-) {
-}
--- src/main/java/org/appjam/bongbaek/domain/member/dto/LoginResponse.java
+++ src/main/java/org/appjam/bongbaek/domain/member/dto/LoginResponse.java
@@ -1,5 +1,6 @@
 package org.appjam.bongbaek.domain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.appjam.bongbaek.global.jwt.dto.TokenResponse;
 
@@ -11,13 +12,21 @@ public record LoginResponse(
         @Schema(description = "회원 가입 완료 여부")
         boolean isCompletedSignUp,
         @Schema(description = "kakao ID", nullable = true)
-        String oauthId
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        String kakaoId,
+        @Schema(description = "apple ID", nullable = true)
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        String appleId
 ) {
-    public static LoginResponse of(final String name, final TokenResponse token, final boolean isCompletedSignUp, final String oauthId) {
-        return new LoginResponse(name, token, isCompletedSignUp, oauthId);
+    public static LoginResponse of(final String name, final TokenResponse token, final boolean isCompletedSignUp, final String kakaoId) {
+        return new LoginResponse(name, token, isCompletedSignUp, kakaoId, null);
     }
 
-    public static LoginResponse ofLoginSuccess(final String name, final TokenResponse token, final String oauthId) {
-        return new LoginResponse(name, token, true, oauthId);
+    public static LoginResponse ofKakaoLoginSuccess(final String name, final TokenResponse token, final String kakaoId) {
+        return new LoginResponse(name, token, true, kakaoId, null);
+    }
+
+    public static LoginResponse ofAppleLoginSuccess(final String name, final TokenResponse token, final String appleId) {
+        return new LoginResponse(name, token, true, null, appleId);
     }
 }
