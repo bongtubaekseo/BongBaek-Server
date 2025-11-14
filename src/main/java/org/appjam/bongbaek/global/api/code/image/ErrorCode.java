295174b5ff4d004adb76d24015a13bec2f256fd6
chounjae <blueskycuj@naver.com>
2025-11-14 12:09:30 +0900

    [FEAT] 이미지 예외 메시지 추가

--- src/main/java/org/appjam/bongbaek/global/api/code/image/ErrorCode.java
+++ src/main/java/org/appjam/bongbaek/global/api/code/image/ErrorCode.java
@@ -9,9 +9,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode implements ErrorResultCode {
+    // 400
+    INVALID_IMAGE_FORMAT(HttpStatus.BAD_REQUEST, "이미지 형식이 올바르지 않습니다"),
     // 404
-    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지 정보가 존재하지 않습니다.");
-
+    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지 정보가 존재하지 않습니다."),
+    // 500
+    FAIL_READ_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "파일 스트림을 읽는 중 오류가 발생했습니다.");
     private final HttpStatus status;
     private final String message;
 }
---
+++ src/main/java/org/appjam/bongbaek/global/exception/handler/image/ImageExceptionHandler.java
@@ -0,0 +1,4 @@
+package org.appjam.bongbaek.global.exception.handler.image;
+
+public class ImageExceptionHandler {
+}
