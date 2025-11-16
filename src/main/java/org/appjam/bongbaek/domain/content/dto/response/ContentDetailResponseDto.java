e7a2c69f5f936475184716ef345bb687edaefd69
chounjae <blueskycuj@naver.com>
2025-11-16 15:37:18 +0900

    [FIX] 상세 조회 category, 생성일자 출력

--- src/main/java/org/appjam/bongbaek/domain/content/dto/response/ContentDetailResponseDto.java
+++ src/main/java/org/appjam/bongbaek/domain/content/dto/response/ContentDetailResponseDto.java
@@ -1,26 +1,30 @@
 package org.appjam.bongbaek.domain.content.dto.response;
 
 import org.appjam.bongbaek.domain.content.entity.Content;
-import org.appjam.bongbaek.domain.event.entity.Category;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record ContentDetailResponseDto(
             String contentId,
             String contentTitle,
-            Category contentCategory,
+            String contentCategory,
             String thumbnailUrl,
             String mainImageUrl,
-            LocalDateTime createdAt
+            String createdAt
 ) {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
     public static ContentDetailResponseDto from(Content content) {
+        String createdAt = content.getCreatedDateTime()
+                .format(DATE_FORMATTER);
+
         return new ContentDetailResponseDto(
                 content.getContentId(),
                 content.getContentTitle(),
-                content.getContentCategory(),
+                content.getContentCategory().getDescription(),
                 content.getThumbnailUrl(),
                 content.getMainImageUrl(),
-                content.getCreatedDateTime()
+                createdAt
         );
     }
 }
