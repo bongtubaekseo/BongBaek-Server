b939c4ef9aedfe1a311d48a71a4d225abaaafb6d
chounjae <blueskycuj@naver.com>
2025-11-16 14:42:04 +0900

    [FEAT] queryDSL없이 JPA로 검색 메서드 구현

--- src/main/java/org/appjam/bongbaek/domain/content/repository/ContentRepository.java
+++ src/main/java/org/appjam/bongbaek/domain/content/repository/ContentRepository.java
@@ -1,13 +1,23 @@
 package org.appjam.bongbaek.domain.content.repository;
 
 import org.appjam.bongbaek.domain.content.entity.Content;
+import org.appjam.bongbaek.domain.event.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ContentRepository extends JpaRepository<Content, String> {
 
     @Query("SELECT c FROM Content c LEFT JOIN FETCH c.contentImage WHERE c.contentId = :contentId")
     Optional<Content> findByIdWithContentImage(String contentId);
+
+    List<Content> findTop3ByOrderByCreatedDateTimeDesc();
+
+    Page<Content> findAllByOrderByCreatedDateTimeDesc(Pageable pageable);
+
+    Page<Content> findContentsByContentCategoryOrderByCreatedDateTimeDesc(Category category, Pageable pageable);
 }
