5f1f9a4fae95f55b91305b5587cfdf9f40e2c113
chounjae <blueskycuj@naver.com>
2025-11-17 15:06:36 +0900

    [FEAT] content 생성일자 NPE 방지를 위해 NotNull 추가

--- src/main/java/org/appjam/bongbaek/domain/common/BaseEntity.java
+++ src/main/java/org/appjam/bongbaek/domain/common/BaseEntity.java
@@ -2,6 +2,7 @@ package org.appjam.bongbaek.domain.common;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,6 +17,7 @@ import lombok.Getter;
 public abstract class BaseEntity {
 
 	@CreatedDate
+	@NotNull
 	private LocalDateTime createdDateTime;
 
 	@LastModifiedDate
