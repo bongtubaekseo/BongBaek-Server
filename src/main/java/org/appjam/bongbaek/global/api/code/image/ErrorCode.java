f9a86ad49021a69f5dced6db281fec0f52ea6fef
chounjae <blueskycuj@naver.com>
2025-11-18 17:14:56 +0900

    [FEAT] 예외 메시지 세분화

--- src/main/java/org/appjam/bongbaek/global/api/code/image/ErrorCode.java
+++ src/main/java/org/appjam/bongbaek/global/api/code/image/ErrorCode.java
@@ -10,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode implements ErrorResultCode {
     // 400
-    INVALID_IMAGE_FORMAT(HttpStatus.BAD_REQUEST, "이미지 형식이 올바르지 않습니다"),
+    INVALID_IMAGE_FORMAT(HttpStatus.BAD_REQUEST, "이미지 확장자가 올바르지 않습니다"),
+    INVALID_IMAGE_REQUEST(HttpStatus.BAD_REQUEST, "이미지 요청 형식이 올바르지 않습니다"),
+
     // 404
     IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지 정보가 존재하지 않습니다."),
+
     // 500
     FAIL_READ_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "파일 스트림을 읽는 중 오류가 발생했습니다.");
     private final HttpStatus status;
