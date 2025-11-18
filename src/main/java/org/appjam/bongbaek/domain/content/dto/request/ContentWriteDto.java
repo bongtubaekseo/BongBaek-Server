6e763e68536c638346c8312f4cd569605646820f
chounjae <blueskycuj@naver.com>
2025-11-18 17:36:10 +0900

    [FEAT] 입력 유효성 dto에서 처리

--- src/main/java/org/appjam/bongbaek/domain/content/dto/request/ContentWriteDto.java
+++ src/main/java/org/appjam/bongbaek/domain/content/dto/request/ContentWriteDto.java
@@ -1,7 +1,11 @@
 package org.appjam.bongbaek.domain.content.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record ContentWriteDto(
+        @NotNull
         String contentTitle,
+        @NotNull
         String contentCategory
 ) {
 }
