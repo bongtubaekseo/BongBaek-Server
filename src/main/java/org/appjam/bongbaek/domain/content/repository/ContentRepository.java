3319f27ebe625bae795a569643cf4317543b2c35
chounjae <blueskycuj@naver.com>
2025-11-18 18:33:04 +0900

    [DELETE] 필요없는 메서드 삭제

--- src/main/java/org/appjam/bongbaek/domain/content/repository/ContentRepository.java
+++ src/main/java/org/appjam/bongbaek/domain/content/repository/ContentRepository.java
@@ -5,16 +5,10 @@ import org.appjam.bongbaek.domain.event.entity.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ContentRepository extends JpaRepository<Content, String> {
-
-    @Query("SELECT c FROM Content c LEFT JOIN FETCH c.contentImage WHERE c.contentId = :contentId")
-    Optional<Content> findByIdWithContentImage(String contentId);
-
     List<Content> findTop3ByOrderByCreatedDateTimeDesc();
 
     Page<Content> findAllByOrderByCreatedDateTimeDesc(Pageable pageable);
