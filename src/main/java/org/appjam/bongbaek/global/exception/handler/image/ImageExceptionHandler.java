ba0ee4031f9b6af0e0cf54ad83e86cbea66e5905
chounjae <blueskycuj@naver.com>
2025-11-18 17:19:40 +0900

    [FEAT] 이미지, 컨텐츠 핸들러 작성

--- src/main/java/org/appjam/bongbaek/global/exception/handler/image/ImageExceptionHandler.java
+++ src/main/java/org/appjam/bongbaek/global/exception/handler/image/ImageExceptionHandler.java
@@ -1,4 +1,39 @@
 package org.appjam.bongbaek.global.exception.handler.image;
 
-public class ImageExceptionHandler {
+import org.appjam.bongbaek.global.api.response.ApiResponse;
+import org.appjam.bongbaek.global.exception.content.ContentNotFoundException;
+import org.appjam.bongbaek.global.exception.handler.BaseExceptionHandler;
+import org.appjam.bongbaek.global.exception.image.FileUploadFailedException;
+import org.appjam.bongbaek.global.exception.image.ImageNotFoundException;
+import org.appjam.bongbaek.global.exception.image.InvalidImageFormatException;
+import org.appjam.bongbaek.global.exception.image.InvalidImageRequestException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ImageExceptionHandler extends BaseExceptionHandler {
+    @ExceptionHandler(FileUploadFailedException.class)
+    protected ApiResponse handleContentNotFoundException(ContentNotFoundException e) {
+        return buildErrorResponse(e.getErrorResultCode());
+    }
+
+    @ExceptionHandler(ImageNotFoundException.class)
+    protected ApiResponse handleImageNotFoundException(ImageNotFoundException e) {
+        return buildErrorResponse(e.getErrorResultCode());
+    }
+
+    @ExceptionHandler(InvalidImageRequestException.class)
+    protected ApiResponse handleInvalidImageRequestException(InvalidImageRequestException e) {
+        return buildErrorResponse(e.getErrorResultCode());
+    }
+
+    @ExceptionHandler(ImageNotFoundException.class)
+    protected ApiResponse handleContentNotFoundException(ImageNotFoundException e) {
+        return buildErrorResponse(e.getErrorResultCode());
+    }
+
+    @ExceptionHandler(InvalidImageFormatException.class)
+    protected ApiResponse handleInvalidImageFormatException(InvalidImageFormatException e) {
+        return buildErrorResponse(e.getErrorResultCode());
+    }
 }
